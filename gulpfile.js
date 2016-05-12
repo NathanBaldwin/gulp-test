c4b4bf7d021d661d9c08d01b13d65401f259484c
Nathan Baldwin <nathansbaldwin@gmail.com>
2016-05-12 17:04:29 -0500

    gulp file constructed and live reload works

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp'),
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync');
+  concat = require('gulp-concat'),
+  filter      = require('gulp-filter');
+
+gulp.task('browser-sync', function() {
+  var files = [
+    './Javascripts/**',
+    // './css/**'
+   ];
+
+  browserSync.init(files, {
+    // files: 'Javascripts/**',
+    server: {
+            baseDir: './'
+    }
+    // server: "./",
+    // proxy: "http://127.0.0.1:8081/gulp-test"
+  });
+});
+
+gulp.task('styles', function() {
+  return sass('sass/*.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('updateJS', function() {
+  return gulp.src('./Javascripts/**')
+    .pipe(browserSync.stream({match: '**/*.js'}))
+})
+
+
+gulp.task('watch', function() {
+  gulp.watch('sass/**', ['styles'])
+  gulp.watch('Javascripts/**', ['updateJS'])
+
+});
+
+gulp.task('default', ['styles', 'browser-sync', 'watch']);
---
+++ javascripts/index.js
@@ -0,0 +1,9 @@
+console.log("page loaded");
+console.log("next logs now yo");
+console.log("howdy");
+console.log("howdy");
+console.log("howdy hey hey HOHOH");
+
+console.log("new log at end HEY");
+console.log("new log at end HEY");
+console.log("new log at end HEY");
